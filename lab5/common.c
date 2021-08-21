4ef349fc7e47e33f0dab1e4fc0375202e47b19a3
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-08-21 19:09:14 +0300

    Create common.c

---
+++ lab5/common.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* get_str(FILE *file) {
+    char *str = NULL;
+    char buf[81];
+    size_t size = 0;
+    int res;
+    do {
+        res = fscanf(file, "%80[^\n]", buf);
+        if (res == 0) {
+            fscanf(file, "%*c"); 
+            if (!str) {
+                str = malloc(sizeof(char));
+                str[0] = '\0';
+            }
+        } else if (res > 0) {
+            size_t len = strlen(buf);
+            str = realloc(str, (size+len+1)*sizeof(char));
+            memcpy(str+size, buf, len);
+            size += len;
+            str[size] = '\0';
+        }
+    } while (res > 0);
+    return str;
+}
+
+int getInt() {
+    int number;
+    int result;
+    do {
+        result = scanf("%d", &number);
+        scanf("%*[^\n]s");
+        scanf("%*c");
+        if (result != 1) {
+            printf("Ошибка! Повторите ввод\n");
+        }
+    } while (result != 1);
+    return number;
+}
+
+float getFloat() {
+    float number;
+    int result;
+    do {
+        result = scanf("%f", &number);
+        scanf("%*[^\n]s");
+        scanf("%*c");
+        if (result != 1) {
+            printf("Ошибка! Повторите ввод\n");
+        }
+    } while (result != 1);
+    return number;
+}
